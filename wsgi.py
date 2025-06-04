ff28de708bde781d0750e731fa70835133c25147
Nicolò Carcagnì <nicolo.carcagni@mail.com>
2025-06-04 22:44:02 +0200

    fix

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from .main import bot  # Assicurati di importare il tuo bot
-
-def application(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    start_response(status, headers)
-    
-    return [b"Bot is running"]
